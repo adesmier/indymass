0e054eb15c3314b15ae68bbca10a00159830c9a2
Anthony Desmier <adesmier@gmail.com>
2016-11-29 22:05:16 +0000

    grid animations

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,45 @@
-var options = {
-  useEasing : true,
-  useGrouping : true,
-  separator : ',',
-  decimal : '.',
-  prefix : '',
-  suffix : ''
-};
 
-var demo = new CountUp("day-counter", 0, 12, 0, 2.5, options);
-demo.start();
+
+$(function(){
+
+    var options = {
+      useEasing : true,
+      useGrouping : true,
+      separator : ',',
+      decimal : '.',
+      prefix : '',
+      suffix : ''
+    };
+
+    var demo = new CountUp("day-counter", 0, 12, 0, 2.5, options);
+    demo.start();
+
+    var date = new Date();
+    var day = date.getDate();
+
+    $('.calender-item').bind('click', function(){
+
+        var clickedDay = $(this).attr('data-day');
+
+        if(clickedDay <= 8){
+            if($(this).siblings().hasClass('flipped')){
+                $(this).siblings().removeClass('flipped');
+            }
+            $(this).toggleClass('flipped');
+        } else {
+            if($(this).siblings().hasClass('flipped')){
+                $(this).siblings().removeClass('flipped');
+            }
+            $(this).addClass('shake').delay(800).queue(function(next){
+                $(this).removeClass('shake');
+                next();
+            })
+        }
+
+
+
+
+
+    })
+
+});
